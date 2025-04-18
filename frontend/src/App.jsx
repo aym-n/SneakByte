acb9b633e2561128f6d81ee1f6181c9160083973
ankrit <jarngalankrit@gmail.com>
2025-04-18 13:10:43 +0530

    basic frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// App.jsx
+import { useState } from 'react'
+import SnakeGame from '../component/Snake' // Make sure this path is correct
+
+function App() {
+  return (
+    <div className="app">
+      <SnakeGame />
+    </div>
+  )
+}
+
+export default App
